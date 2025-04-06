b8219eb03ef86930d88d283a53465ac62b4dff9c
miguel.jaeger <miguel.jaeger@gmail.com>
2025-04-06 16:32:03 -0500

    add function to edit an remove user

--- crud/src/main/java/com/crud/persona/crud/controlador/ControladorUsuario.java
+++ crud/src/main/java/com/crud/persona/crud/controlador/ControladorUsuario.java
@@ -39,5 +39,36 @@ public class ControladorUsuario<Usuario> {
        return "redirect:/usuarios";
    }
    
+   @GetMapping("usuarios/editar/{id}")
+   public String mostrarFormularioEditar(@RequestParam Long idPersona, Model modelo) {
+       modelo.addAttribute("usuario", servicioUsuario.obtenerUsuarioPorId(idPersona));
+       return "editarUsuario";
+   }
+ 
+   @PostMapping("usuarios/{id}")
+   public String actualizarUsuario(@RequestParam Long idPersona, @ModelAttribute("usuario") ModeloUsuario usuario) {
+       // Obtener el usuario existente de la base de datos
+       ModeloUsuario usuarioExistente = servicioUsuario.obtenerUsuarioPorId(idPersona);
+       // Actualizar los campos del usuario existente con los nuevos valores
+      usuarioExistente.setIdPersona(idPersona);
+       usuarioExistente.setNombre(usuario.getNombre());
+       usuarioExistente.setApellido(usuario.getApellido());
+       usuarioExistente.setCorreo(usuario.getCorreo());
+      // usuarioExistente.setContrasena(usuario.getContrasena());
+       // Guardar el usuario actualizado en la base de datos
+       servicioUsuario.actualizarUsuario(usuarioExistente);
+       return "redirect:/usuarios";
+    
+   }
+
+   @GetMapping("usuarios/eliminar/{id}")
+   public String eliminarUsuario(@RequestParam Long idPersona) {
+     ModeloUsuario usuario = new ModeloUsuario();   
+         usuario.setIdPersona(idPersona);
+       servicioUsuario.eliminarUsuario(usuario);
+       return "redirect:/usuarios";
+   }
+ 
+   
 
 }
