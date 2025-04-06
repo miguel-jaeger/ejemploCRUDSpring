3832205e331c368a64149bad83cfa24f636ee6da
miguel.jaeger <miguel.jaeger@gmail.com>
2025-04-06 15:39:05 -0500

    add function to list and add new user

--- crud/src/main/java/com/crud/persona/crud/controlador/ControladorUsuario.java
+++ crud/src/main/java/com/crud/persona/crud/controlador/ControladorUsuario.java
@@ -4,30 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.crud.persona.crud.modelo.ModeloUsuario;
 import com.crud.persona.crud.servicios.ServicioUsuario;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
-public class ControladorUsuario {
+public class ControladorUsuario<Usuario> {
     @Autowired
     private ServicioUsuario servicioUsuario;
-    
-    //Listar
-      @GetMapping("/usuarios")
-        public String listarUsuarios(Model modelo) {
-           modelo.addAttribute("usuarios", servicioUsuario.listarUsuarios());
-            return "usuarios";
-        }
 
-        @GetMapping("usuarios/nuevo")
-        public String mostrarFormularioAdicionar(Model modelo) {
-            ModeloUsuario usuario = new ModeloUsuario();
-            modelo.addAttribute("usuario", usuario);            
-            return "adicionarUsuario";
-        }
-        
-        
+    // Listar
+    @GetMapping("/usuarios")
+    public String listarUsuarios(Model modelo) {
+        modelo.addAttribute("usuarios", servicioUsuario.listarUsuarios());
+        return "usuarios";
+    }
+
+    @GetMapping("usuarios/nuevo")
+    public String mostrarFormularioAdicionar(Model modelo) {
+        ModeloUsuario usuario = new ModeloUsuario();
+        modelo.addAttribute("usuario", usuario);
+        return "adicionarUsuario";
+    }
+
+    // Guardar usuario
+   @PostMapping("usuarios/guardar")
+   public String guardarUsuario(@ModelAttribute("usuario") ModeloUsuario usuario) {
+       servicioUsuario.guardarUsuario(usuario);
+       return "redirect:/usuarios";
+   }
+   
+
 }
