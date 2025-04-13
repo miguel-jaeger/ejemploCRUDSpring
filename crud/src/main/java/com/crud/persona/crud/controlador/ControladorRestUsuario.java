defdcfee93e4e2a8b6c546dd6abf8e12cc7e9b06
miguel.jaeger <miguel.jaeger@gmail.com>
2025-04-13 12:18:26 -0500

    add encrypt password functions

--- crud/src/main/java/com/crud/persona/crud/controlador/ControladorRestUsuario.java
+++ crud/src/main/java/com/crud/persona/crud/controlador/ControladorRestUsuario.java
@@ -2,6 +2,7 @@ package com.crud.persona.crud.controlador;
 
 import java.util.ArrayList;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.crud.persona.crud.servicios.ServicioUsuario;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -34,12 +36,13 @@ public class ControladorRestUsuario {
 //Adicionar
     @PostMapping
     public ModeloUsuario salvarUsuario(@RequestBody ModeloUsuario usuario) {
-         
+        usuario.setContrasena(this.claveEncriptada(usuario.getContrasena()));      
         return this.servicioUsuario.guardarUsuario(usuario);
 }   
 
 @PutMapping
 public ModeloUsuario actualizarUsuario(@RequestBody ModeloUsuario usuario) {
+    usuario.setContrasena(this.claveEncriptada(usuario.getContrasena()));      
     return this.servicioUsuario.actualizarUsuario(usuario);
 }
 
@@ -48,4 +51,7 @@ public void eliminarUsuario(@RequestBody ModeloUsuario usuario) {
     this.servicioUsuario.eliminarUsuario(usuario);
 }
 
+ public String claveEncriptada(String clave) {
+	   return BCrypt.hashpw(clave, BCrypt.gensalt());
+   }
 }
