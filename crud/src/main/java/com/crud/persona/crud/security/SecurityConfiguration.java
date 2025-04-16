41d2bd9eb9dcffbb5411dfc3cc1aac8c80cfedef
miguel.jaeger <miguel.jaeger@gmail.com>
2025-04-16 08:56:00 -0500

    change to remove security

--- crud/src/main/java/com/crud/persona/crud/security/SecurityConfiguration.java
+++ crud/src/main/java/com/crud/persona/crud/security/SecurityConfiguration.java
@@ -6,10 +6,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder;
 
 /**
  * An example of explicitly configuring Spring Security with the defaults.
@@ -23,26 +26,35 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		// @formatter:off
-		http
+		/*http
 				.authorizeHttpRequests((authorize) -> authorize
 						.anyRequest().authenticated() //permitAll() -> colocar para desactivar el security
 				)
 				.httpBasic(withDefaults()) //comentar para desactivar el security
 				.formLogin(withDefaults()); //comentar para desactivar el security
 		// @formatter:on
-		return http.build();
+		return http.build();*/
+		http
+		.csrf(csrf -> csrf.disable())
+		.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+	return http.build();
 	}
 
 	// @formatter:off
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
-	}
+	   @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+	  @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 	// @formatter:on
 
 }
