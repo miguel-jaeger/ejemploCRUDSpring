65ce1b2ea4e05323261f0da2abbd18aa987483a3
miguel.jaeger <miguel.jaeger@gmail.com>
2025-04-20 11:26:04 -0500

    change user repository to get all roles by user

--- crud/src/main/java/com/crud/persona/crud/repositorios/IRepositorioUsuario.java
+++ crud/src/main/java/com/crud/persona/crud/repositorios/IRepositorioUsuario.java
@@ -1,12 +1,19 @@
 package com.crud.persona.crud.repositorios;
 
+
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.crud.persona.crud.modelo.ModeloUsuario;
 
 @Repository
 public interface IRepositorioUsuario extends JpaRepository<ModeloUsuario, Long> {
 	ModeloUsuario findByCorreo(String nombreUsuario);
+
+	@Query("SELECT u FROM ModeloUsuario u JOIN FETCH u.rol WHERE u.idPersona = :id")
+    Optional<ModeloUsuario> findByIdWithRol(@Param("id") Long id);
 }
